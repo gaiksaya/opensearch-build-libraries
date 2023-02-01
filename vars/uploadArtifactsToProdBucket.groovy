19c7962edbb7ebb51ef21758c5f7eb174a3d1287
Sayali Gaikawad <gaiksaya@amazon.com>
2023-01-31 17:36:15 -0800

    Add code
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- vars/uploadArtifactsToProdBucket.groovy
+++ vars/uploadArtifactsToProdBucket.groovy
@@ -10,10 +10,8 @@
  /**
  * Library to upload artifacts to artifacts.opensearch.org
 @param Map[roleName] <required> - IAM role to be assumed for uploading artifacts
-@param Map[srcArtifactDir] <required> - Path to yml or artifact file.
-@param Map[DestArtifactDir] <required> - Artifact type in the manifest, [type] is required for signing yml.
-@param Map[sigtype] <Optional> - The signature type of signing artifacts. e.g. '.sig'. Required for non-yml artifacts signing.
-@param Map[platform] <Required> - The distribution platform for signing.
+@param Map[source] <required> - Path to yml or artifact file.
+@param Map[destination] <required> - Artifact type in the manifest, [type] is required for signing yml.
  */
 void call(Map args = [:]) {
     withCredentials([
@@ -21,8 +19,7 @@ void call(Map args = [:]) {
         string(credentialsId: 'jenkins-aws-production-account', variable: 'AWS_ACCOUNT_ARTIFACT'),
         string(credentialsId: 'jenkins-artifact-production-bucket-name', variable: 'ARTIFACT_PRODUCTION_BUCKET_NAME')]) {
             withAWS(role: "${ARTIFACT_PROMOTION_ROLE_NAME}", roleAccount: "${AWS_ACCOUNT_ARTIFACT}", duration: 900, roleSessionName: 'jenkins-session') {
-                s3Upload(file: "${srcDir}/${baseName}-latest.${extension}", bucket: "${ARTIFACT_PRODUCTION_BUCKET_NAME}", path: "${dstDir}/${baseName}-latest.${extension}")
-                s3Upload(file: "${srcDir}/${baseName}-latest.${extension}.sha512", bucket: "${ARTIFACT_PRODUCTION_BUCKET_NAME}", path: "${dstDir}/${baseName}-latest.${extension}.sha512")
-            }
+                s3Upload(file: "${source}", bucket: "${ARTIFACT_PRODUCTION_BUCKET_NAME}", path: "${destination}")
         }
+    }
 }
