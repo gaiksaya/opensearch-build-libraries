8fc088912833e1ac5b4d4ff6c11d7f84977d3cdd
Sayali Gaikawad <gaiksaya@amazon.com>
2023-02-01 12:17:51 -0800

    test
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- vars/publishToRubyGems.groovy
+++ vars/publishToRubyGems.groovy
@@ -23,7 +23,7 @@ void call(Map args = [:]) {
     String rubyVersion = args.rubyVersion ?: '2.6.0'
 
     sh """
-        source ~/.rvm/scripts/rvm && rvm use ${rubyVersion}
+        bash --login && rvm use ${rubyVersion} && ruby --version
         gem cert --add ${certPath}
         cd ${releaseArtifactsDir} && gemNameWithVersion=\$(ls *.gem)
         gem install \$gemNameWithVersion
