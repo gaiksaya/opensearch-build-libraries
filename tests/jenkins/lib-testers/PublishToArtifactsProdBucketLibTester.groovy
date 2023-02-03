303f854199897581ee712bc14fccabc92c24bd65
Sayali Gaikawad <gaiksaya@amazon.com>
2023-02-02 16:18:49 -0800

    Add lib to publish to artifacts.opensearch.org
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

---
+++ tests/jenkins/lib-testers/PublishToArtifactsProdBucketLibTester.groovy
@@ -0,0 +1,68 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+import static org.hamcrest.CoreMatchers.notNullValue
+import static org.hamcrest.MatcherAssert.assertThat
+
+class PublishToArtifactsProdBucketLibTester extends LibFunctionTester {
+
+    private String roleName
+    private String source
+    private String destination
+    private String signingPlatform
+    private String sigtype
+    private boolean overwrite
+
+    public PublishToArtifactsProdBucketLibTester(roleName, source, destination, signingPlatform, sigtype, overwrite) {
+        this.roleName = roleName
+        this.source = source
+        this.destination = destination
+        this.sigtype = sigtype
+        this.signingPlatform = signingPlatform
+        this.overwrite = overwrite
+    }
+
+    public PublishToArtifactsProdBucketLibTester(roleName, source, destination){
+        this.roleName = roleName
+        this.source = source
+        this.destination = destination
+    }
+
+    void configure(helper, binding){
+        helper.registerAllowedMethod("s3Upload", [Map])
+        binding.setVariable('GITHUB_BOT_TOKEN_NAME', 'github_bot_token_name')
+        helper.registerAllowedMethod('git', [Map])
+        helper.registerAllowedMethod('withCredentials', [Map, Closure], { args, closure ->
+            closure.delegate = delegate
+            return helper.callClosure(closure)
+        })
+        helper.registerAllowedMethod('withAWS', [Map, Closure], { args, closure ->
+            closure.delegate = delegate
+            return helper.callClosure(closure)
+        })
+    }
+
+    void parameterInvariantsAssertions(call){
+        assertThat(call.args.roleName.first(), notNullValue())
+        assertThat(call.args.source.first(), notNullValue())
+        assertThat(call.args.destination.first(), notNullValue())
+        // assertThat(call.args.sigtype.first(), notNullValue())
+        // assertThat(call.args.signingPlatform.first(), notNullValue())
+        // assertThat(call.args.overwrite.first(), notNullValue())
+    }
+
+    boolean expectedParametersMatcher(call) {
+        return call.args.roleName.first().toString().equals(this.roleName)
+                && call.args.source.first().toString().equals(this.source)
+                && call.args.destination.first().toString().equals(this.destination)
+    }
+
+    String libFunctionName() {
+        return 'publishToArtifactsProdBucket'
+    }
+}
--- vars/publishToArtifactsProdBucket.groovy
+++ vars/publishToArtifactsProdBucket.groovy
@@ -8,18 +8,29 @@
  */
 
  /**
- * Library to upload artifacts to artifacts.opensearch.org
+ * Library to sign and upload artifacts to artifacts.opensearch.org
 @param Map[roleName] <required> - IAM role to be assumed for uploading artifacts
 @param Map[source] <required> - Path to yml or artifact file.
 @param Map[destination] <required> - Artifact type in the manifest, [type] is required for signing yml.
+@param Map[sigtype] <optional> - signature type. Defaults to '.sig'
+@param Map[signingPlatform] <optional> - The distribution platform for signing. Defaults to linux
+@param Map[overwrite]<optional> - Allow output artifacts to overwrite the existing artifacts. Defaults to false
  */
 void call(Map args = [:]) {
+    lib = library(identifier: 'jenkins@main', retriever: legacySCM(scm))
+    println('Signing the artifacts')
+    signArtifacts(
+            artifactPath: args.source,
+            platform: args.signingPlatform ?: 'linux',
+            sigtype: args.sigType ?: '.sig',
+            overwrite: args.overwrite ?: false
+            )
     withCredentials([
         string(credentialsId: "${args.roleName}", variable: 'ARTIFACT_PROMOTION_ROLE_NAME'),
         string(credentialsId: 'jenkins-aws-production-account', variable: 'AWS_ACCOUNT_ARTIFACT'),
         string(credentialsId: 'jenkins-artifact-production-bucket-name', variable: 'ARTIFACT_PRODUCTION_BUCKET_NAME')]) {
             withAWS(role: "${ARTIFACT_PROMOTION_ROLE_NAME}", roleAccount: "${AWS_ACCOUNT_ARTIFACT}", duration: 900, roleSessionName: 'jenkins-session') {
-                s3Upload(file: "${source}", bucket: "${ARTIFACT_PRODUCTION_BUCKET_NAME}", path: "${destination}")
+                s3Upload(file: "${args.source}", bucket: "${ARTIFACT_PRODUCTION_BUCKET_NAME}", path: "${args.destination}")
         }
     }
 }
