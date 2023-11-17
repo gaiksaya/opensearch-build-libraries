4cde1c51c4496e38dfa21cd06b80aaddc704c643
Sayali Gaikawad <gaiksaya@amazon.com>
2023-11-17 15:50:17 -0800

    fix
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- vars/logParserForStage.groovy
+++ vars/logParserForStage.groovy
@@ -30,6 +30,7 @@ String getFlowNodeStage(FlowNode flowNode) {
 // Collect logs of each flow node that belongs to stage
 @NonCPS
 List<String> collectLogsForStage(String stageName) {
+    println(currentBuild.rawBuild)
     currentBuild.rawBuild.save()
     List<String> logs = []
     DepthFirstScanner scanner = new DepthFirstScanner()
