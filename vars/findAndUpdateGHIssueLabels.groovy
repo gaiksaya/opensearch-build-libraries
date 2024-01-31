cff25812900b938e72b3b2066c4d1f9c50239019
Sayali Gaikawad <gaiksaya@amazon.com>
2024-01-30 17:20:44 -0800

    test
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- vars/findAndUpdateGHIssueLabels.groovy
+++ vars/findAndUpdateGHIssueLabels.groovy
@@ -24,7 +24,7 @@ void call(Map args = [:]) {
                     returnStdout: true
             ).trim()
             if (!issueNumber.isEmpty()) {
-                if (labelExists) {
+                if (labelExists.toBoolean()) {
                     sh(
                             script: "gh issue edit ${issueNumber} -R ${args.repoUrl} ${action} \"${args.label}\"",
                             returnStdout: true
