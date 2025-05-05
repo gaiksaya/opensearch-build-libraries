e5af592ae6fc09e36c4cee1596e638f2b47502e2
Sayali Gaikawad <gaiksaya@amazon.com>
2025-05-05 16:56:42 -0700

    test
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- vars/handleVersionIncrementPRs.groovy
+++ vars/handleVersionIncrementPRs.groovy
@@ -29,6 +29,9 @@ void call(Map args = [:]) {
         withAWS(role: 'OpenSearchJenkinsAccessRole', roleAccount: "${METRICS_HOST_ACCOUNT}", duration: 900, roleSessionName: 'jenkins-session') {
             def releaseMetrics = new ReleaseMetricsData(env.METRICS_HOST_URL, env.AWS_ACCESS_KEY_ID, env.AWS_SECRET_ACCESS_KEY, env.AWS_SESSION_TOKEN, version, 'github_pulls', this)
             def pullRequestUrls = releaseMetrics.getVersionIncrementPrsUrls()
+            pullRequestUrls.each { pr ->
+                reRunChecks(pr)
+            }
         }
     }
 
