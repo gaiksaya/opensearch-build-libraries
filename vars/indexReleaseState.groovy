9ed38eacd12264121ff9ef4288545093dc565449
Sayali Gaikawad <gaiksaya@amazon.com>
2026-08-18 20:57:51 -0700

    bisect: stub body, keep imports
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- vars/indexReleaseState.groovy
+++ vars/indexReleaseState.groovy
@@ -3,6 +3,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import jenkins.ReleaseStateData
+import jenkins.ReleaseCriterion
+
 void call(Map args = [:]) {
     echo("Indexing release state for version 3.8.0.")
 }
