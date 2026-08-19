f1885eb80627a4c09064f06a8c99e764dce2cfb1
Sayali Gaikawad <gaiksaya@amazon.com>
2026-08-18 22:38:45 -0700

    bisect: construct both RSD and RC, call indexCriterion
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- vars/indexReleaseState.groovy
+++ vars/indexReleaseState.groovy
@@ -3,20 +3,12 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import jenkins.ReleaseStateData
+import jenkins.ReleaseCriterion
+
 void call(Map args = [:]) {
+    def rsd = new ReleaseStateData('u', 'a', 's', 't', this)
+    def rc = new ReleaseCriterion([version: '3.8.0', criterionType: 'exit', criterionName: 'x', status: 'met'])
+    rsd.indexCriterion(rc)
     echo("Indexing release state for version 3.8.0.")
-}
-
-private String renderDetails(Map<String, List<String>> problemsByKey) {
-    return problemsByKey.collect { key, items -> "${key}: ${items.join(', ')}" }.join('; ')
-}
-
-private Map renderVulnerabilityResults(Map<String, List<String>> byProject) {
-    return [blockingComponents: byProject.keySet().toList(), details: renderDetails(byProject)]
-}
-
-private Map renderIntegResults(Map<String, List<String>> byDistArch) {
-    Map<String, List<String>> failing = byDistArch.findAll { distArch, components -> components }
-    List<String> components = failing.values().flatten().unique()
-    return [blockingComponents: components, details: failing ? renderDetails(failing) : null]
 }
